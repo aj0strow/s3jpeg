9081359c4b1f7c4b9cef206ffc19ca5b571e2f24
AJ Ostrow <alexander.ostrow@gmail.com>
2014-07-14 15:14:15 -0400

    get image

--- s3jpeg.go
+++ s3jpeg.go
@@ -1,11 +1,15 @@
 package s3jpeg
 
 import (
+  "image"
+  "image/jpeg"
   "github.com/mitchellh/goamz/aws"
   "github.com/mitchellh/goamz/s3"
 )
 
-type Bucket s3.Bucket
+type Bucket struct {
+  s3.Bucket
+}
 
 func New(name string) *Bucket {
   bucket := new(Bucket)
@@ -15,8 +19,17 @@ func New(name string) *Bucket {
   if e == nil {
     s3 := new(s3.S3)
     s3.Auth = auth
+    s3.Region = aws.USEast
     bucket.S3 = s3
   }
 
   return bucket
+}
+
+func (b *Bucket) GetImage(path string) (image.Image, error) {
+  reader, e := b.GetReader(path)
+  if e != nil {
+    return nil, e
+  }
+  return jpeg.Decode(reader)
 }
--- s3jpeg_test.go
+++ s3jpeg_test.go
@@ -10,3 +10,11 @@ func TestNew(t *testing.T) {
     t.Errorf("Coundn't find auth in env.")
   }
 }
+
+func TestGetImage(t *testing.T) {
+  bucket := New("modde-test")
+  jpeg, e := bucket.GetImage("test_image.jpg")
+  if jpeg == nil || e != nil {
+    t.Errorf("Got error %s.", e.Error())
+  }
+}
