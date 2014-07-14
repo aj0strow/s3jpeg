ff521513da113d41f911b6d4dafafabd27cdc99c
AJ Ostrow <alexander.ostrow@gmail.com>
2014-07-14 16:29:44 -0400

    thumbnail resize and fit

--- s3jpeg.go
+++ s3jpeg.go
@@ -1,59 +1,105 @@
 package s3jpeg
 
 import (
-  "image"
-  "image/jpeg"
-  "bytes"
-  "github.com/mitchellh/goamz/aws"
-  "github.com/mitchellh/goamz/s3"
-  "github.com/disintegration/imaging"
+	"bytes"
+	"github.com/disintegration/imaging"
+	"github.com/mitchellh/goamz/aws"
+	"github.com/mitchellh/goamz/s3"
+	"image"
+	"image/jpeg"
+	"io"
 )
 
 type Bucket struct {
-  s3.Bucket
-  ACL s3.ACL
+	s3.Bucket
+	ACL s3.ACL
 }
 
 type Version struct {
-  Width, Height, Quality int
+	Width, Height, Quality int
 }
 
 func New(name string) *Bucket {
-  bucket := new(Bucket)
-  bucket.Name = name
-  bucket.ACL = s3.PublicRead
+	bucket := new(Bucket)
+	bucket.Name = name
+	bucket.ACL = s3.PublicRead
 
-  
-  auth, e := aws.EnvAuth()
-  if e == nil {
-    s3 := new(s3.S3)
-    s3.Auth = auth
-    s3.Region = aws.USEast
-    bucket.S3 = s3
-  }
+	auth, e := aws.EnvAuth()
+	if e == nil {
+		s3 := new(s3.S3)
+		s3.Auth = auth
+		s3.Region = aws.USEast
+		bucket.S3 = s3
+	}
 
-  return bucket
+	return bucket
 }
 
+// GetImage & PutImage
+// wrap s3.Bucket methods to deal with jpeg image files
+
 func (b *Bucket) GetImage(path string) (image.Image, error) {
-  reader, e := b.GetReader(path)
-  if e != nil {
-    return nil, e
-  }
-  return jpeg.Decode(reader)
+	reader, e := b.GetReader(path)
+	if e != nil {
+		return nil, e
+	}
+	return jpeg.Decode(reader)
 }
 
 func (b *Bucket) PutImage(path string, i image.Image, quality int) error {
-  var r bytes.Buffer
-  e := jpeg.Encode(&r, i, &jpeg.Options{ quality })
-  if e != nil {
-    return e
-  }
-  len := int64(r.Len())
-  return b.PutReader(path, &r, len, "image/jpeg", b.ACL)
+	var r bytes.Buffer
+	e := jpeg.Encode(&r, i, &jpeg.Options{quality})
+	if e != nil {
+		return e
+	}
+	len := int64(r.Len())
+	return b.PutReader(path, &r, len, "image/jpeg", b.ACL)
 }
 
+// Resize
+// leave width or height 0 to maintain ratio
+
+func (b *Bucket) PutResize(path string, i image.Image, v *Version) error {
+	img := imaging.Resize(i, v.Width, v.Height, imaging.Lanczos)
+	return b.PutImage(path, img, v.Quality)
+}
+
+func (b *Bucket) PutResizeReader(path string, r io.Reader, v *Version) error {
+	img, e := jpeg.Decode(r)
+	if e != nil {
+		return e
+	}
+	return b.PutResize(path, img, v)
+}
+
+// Fit
+// scale image down to fit within width x height box
+
+func (b *Bucket) PutFit(path string, i image.Image, v *Version) error {
+	img := imaging.Fit(i, v.Width, v.Height, imaging.Lanczos)
+	return b.PutImage(path, img, v.Quality)
+}
+
+func (b *Bucket) PutFitReader(path string, r io.Reader, v *Version) error {
+	img, e := jpeg.Decode(r)
+	if e != nil {
+		return e
+	}
+	return b.PutFit(path, img, v)
+}
+
+// Thumbnail
+// crop image to width and height
+
 func (b *Bucket) PutThumbnail(path string, i image.Image, v *Version) error {
-  img := imaging.Thumbnail(i, v.Width, v.Height, imaging.CatmullRom)
-  return b.PutImage(path, img, v.Quality)
+	img := imaging.Thumbnail(i, v.Width, v.Height, imaging.Lanczos)
+	return b.PutImage(path, img, v.Quality)
+}
+
+func (b *Bucket) PutThumbnailReader(path string, r io.Reader, v *Version) error {
+	img, e := jpeg.Decode(r)
+	if e != nil {
+		return e
+	}
+	return b.PutThumbnail(path, img, v)
 }
--- s3jpeg_test.go
+++ s3jpeg_test.go
@@ -1,49 +1,101 @@
 package s3jpeg
 
 import (
-  "testing"
-  "os"
-  "image"
-  "image/jpeg"
+	"image"
+	"image/jpeg"
+	"os"
+	"testing"
 )
 
 func openImage(fname string) (image.Image, error) {
-  file, e := os.Open(fname)
-  if e != nil {
-    return nil, e
-  }
-  return jpeg.Decode(file)
+	file, e := os.Open(fname)
+	defer file.Close()
+	if e != nil {
+		return nil, e
+	}
+	return jpeg.Decode(file)
 }
 
 func TestNew(t *testing.T) {
-  bucket := New("modde-test")
-  if bucket.S3 == nil {
-    t.Errorf("Coundn't find auth in env.")
-  }
+	bucket := New("modde-test")
+	if bucket.S3 == nil {
+		t.Errorf("Coundn't find auth in env.")
+	}
 }
 
 func TestGetImage(t *testing.T) {
-  bucket := New("modde-test")
-  _, e := bucket.GetImage("test_image.jpg")
-  if e != nil {
-    t.Errorf("Got error %s.", e.Error())
-  }
+	bucket := New("modde-test")
+	_, e := bucket.GetImage("test_image.jpg")
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
 }
 
 func TestPutImage(t *testing.T) {
-  bucket := New("modde-test")
-  img, e := openImage("logo.jpg")
-  e = bucket.PutImage("test/logo.jpg", img, 50)
-  if e != nil {
-    t.Errorf("Got error %s.", e.Error())
-  }
+	bucket := New("modde-test")
+	img, e := openImage("logo.jpg")
+	e = bucket.PutImage("test/logo.jpg", img, 50)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
+}
+
+func TestPutResize(t *testing.T) {
+	bucket := New("modde-test")
+	img, e := openImage("logo.jpg")
+	small := Version{100, 0, 80}
+	e = bucket.PutResize("test/resize/image.jpg", img, &small)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
+}
+
+func TestPutResizeReader(t *testing.T) {
+	bucket := New("modde-test")
+	file, e := os.Open("logo.jpg")
+	medium := Version{100, 0, 80}
+	e = bucket.PutResizeReader("test/resize/reader.jpg", file, &medium)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
+}
+
+func TestPutFit(t *testing.T) {
+	bucket := New("modde-test")
+	img, e := openImage("logo.jpg")
+	small := Version{200, 200, 60}
+	e = bucket.PutFit("test/fit/image.jpg", img, &small)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
+}
+
+func TestPutFitReader(t *testing.T) {
+	bucket := New("modde-test")
+	file, e := os.Open("logo.jpg")
+	medium := Version{200, 200, 60}
+	e = bucket.PutFitReader("test/fit/reader.jpg", file, &medium)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
 }
 
 func TestPutThumbnail(t *testing.T) {
-  bucket := New("modde-test")
-  img, e := openImage("logo.jpg")
-  e = bucket.PutThumbnail("test/logo_small.jpg", img, &Version{ 100, 50, 80 })
-  if e != nil {
-    t.Errorf("Got error %s.", e.Error())
-  }
+	bucket := New("modde-test")
+	img, e := openImage("logo.jpg")
+	small := Version{100, 45, 80}
+	e = bucket.PutThumbnail("test/thumbnail/image.jpg", img, &small)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
+}
+
+func TestPutThumbnailReader(t *testing.T) {
+	bucket := New("modde-test")
+	file, e := os.Open("logo.jpg")
+	medium := Version{100, 45, 90}
+	e = bucket.PutThumbnailReader("test/thumbnail/reader.jpg", file, &medium)
+	if e != nil {
+		t.Errorf("Got error %s.", e.Error())
+	}
 }
